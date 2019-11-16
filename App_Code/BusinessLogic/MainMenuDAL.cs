ba852ba62662261d6a269331fc7b0b449693c2fd
tejaswiindukuri <tejaswi.indukuri@dinoosys.com>
2019-11-16 12:27:23 +0530

    DAL classes
    DAL classes

---
+++ App_Code/BusinessLogic/MainMenuDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections;
+using System.Data;
+using DataManager;
+
+/// <summary>
+/// Summary description for MainMenuDAL
+/// </summary>
+public class MainMenuDAL
+{
+    DataUtilities objUtilities = new DataUtilities();
+    public DataSet GetMainMenuDetails()
+    {
+        return objUtilities.ExecuteDataSet("GetMainMenuDetails");
+    }
+}
---
+++ App_Code/BusinessLogic/SubMenuDAL.cs
@@ -0,0 +1,19 @@
+﻿using DataManager;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for SubMenuDAL
+/// </summary>
+public class SubMenuDAL
+{
+    DataUtilities objUtilities = new DataUtilities();
+    public DataSet GetSubMenuDetails()
+    {
+        return objUtilities.ExecuteDataSet("GetSubMenuDetails");
+    }
+}
